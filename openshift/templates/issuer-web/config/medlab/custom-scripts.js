d5b6eed9447a92082636d4b8285225a143ce46c2
Emiliano Suñé <emiliano.sune@gmail.com>
2020-04-11 20:36:22 -0700

    Update medlab schema and configurations depending on it
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- openshift/templates/issuer-web/config/medlab/custom-scripts.js
+++ openshift/templates/issuer-web/config/medlab/custom-scripts.js
@@ -1,20 +1,50 @@
 /* Include all your custom JS code in here, it will be available to the app instance */
 
-function setExpiryDate(params) {
+function setCurrentISODate(params) {
   if (params.length < 1) {
-    throw new Error("setExpiryDate is missing one or more required parameters");
+    throw new Error(
+      "setCurrentISODate is missing one or more required parameters"
+    );
   }
   var dateField = params[0];
   var survey = this.survey;
 
   var date = new Date();
-  date.setFullYear(
-    date.getFullYear() + (3 + Math.floor(Math.random() * Math.floor(2)))
-  );
-  survey.setValue(dateField, date.toISOString().substr(0, 10));
+  survey.setValue(dateField, date.toISOString());
+}
+
+function setFutureDate(params) {
+  if (params.length < 1) {
+    throw new Error("setFutureDate is missing one or more required parameters");
+  }
+  var dateField = params[0];
+  var survey = this.survey;
+
+  var date = new Date();
+  date.setDate(date.getDate() + 14);
+  survey.setValue(dateField, date.toISOString());
+}
+
+function setCheckTime(params) {
+  if (params.length < 1) {
+    throw new Error("setFutureDate is missing one or more required parameters");
+  }
+  var dateField = params[0];
+  var survey = this.survey;
+
+  var date = new Date();
+
+  var daysDelta = Math.floor(Math.random() * Math.floor(2)) + 1;
+  var hoursDelta = Math.floor(Math.random() * Math.floor(3));
+  var plusOrMinus = Math.random() < 0.5 ? -1 : 1;
+
+  date.setDate(date.getDate() - daysDelta);
+  date.setHours(date.getHours() + hoursDelta * plusOrMinus);
+
+  survey.setValue(dateField, date.toISOString());
 }
 
 /* An array containing custom functions that will be automatically registered with
  * SurveyJS so that they can be used in triggers.
  */
-surveyFunctions = [setExpiryDate];
+surveyFunctions = [setCurrentISODate, setFutureDate, setCheckTime];
