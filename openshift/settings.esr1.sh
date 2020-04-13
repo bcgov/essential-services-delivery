c3e55f77eb2a977ae743f698d32697467a795b3c
Wade Barnes <wade.barnes@shaw.ca>
2020-04-13 13:17:09 -0700

    Exclude esr-web configurations from the issuer profiles.
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

--- openshift/settings.esr1.sh
+++ openshift/settings.esr1.sh
@@ -1,2 +1,2 @@
 export SKIP_PIPELINE_PROCESSING=1
-export ignore_templates="visual-verifier-build visual-verifier-deploy"
+export ignore_templates="visual-verifier-build visual-verifier-deploy esr-web-base-build esr-web-build esr-web-deploy"
