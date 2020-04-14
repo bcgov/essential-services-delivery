2164e6d6fbeab50bbdd1b08c7d7babfa72d6f531
Wade Barnes <wade.barnes@shaw.ca>
2020-04-14 14:05:23 -0700

    Add `safe-entry` build, deployment, and pipeline configurations
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

---
+++ jenkins/safe-entry/config.groovy
@@ -0,0 +1,10 @@
+// Pipeline Configuration Properties
+// Import this file into the pipeline using 'load'.
+class config extends bc.baseConfig {
+  // Build configuration
+  public static final String  APP_NAME = "safe-entry"
+  public static String  BASE_IMAGE_NAME = "${this.APP_NAME}-base-image"
+  public static final String[] BUILDS = ["${this.BASE_IMAGE_NAME}", "${this.APP_NAME}"]
+}
+
+return new config();
