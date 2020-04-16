b85e0d4a5d37e5267daf1957a1df6b957f05c8f2
Wade Barnes <wade.barnes@shaw.ca>
2020-04-16 15:30:20 -0700

    Integrate rocket.chat notifications into the pipelines
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

--- vars/tagImage.groovy
+++ vars/tagImage.groovy
@@ -2,13 +2,20 @@ void call(String imageName, String tag) {
   openshift.withCluster() {
     openshift.withProject() {
 
-      echo "Tagging ${imageName} for deployment to ${tag} ..."
+      try {
+        echo "Tagging ${imageName} for deployment to ${tag} ..."
 
-      // Don't tag with BUILD_ID so the pruner can do it's job; it won't delete tagged images.
-      // Tag the images for deployment based on the image's hash
-      def IMAGE_HASH = getImageTagHash(openshift, "${imageName}")
-      echo "IMAGE_HASH: ${IMAGE_HASH}"
-      openshift.tag("${imageName}@${IMAGE_HASH}", "${imageName}:${tag}")
+        // Don't tag with BUILD_ID so the pruner can do it's job; it won't delete tagged images.
+        // Tag the images for deployment based on the image's hash
+        def IMAGE_HASH = getImageTagHash(openshift, "${imageName}")
+        echo "IMAGE_HASH: ${IMAGE_HASH}"
+        openshift.tag("${imageName}@${IMAGE_HASH}", "${imageName}:${tag}")
+      } catch(err) {
+        notifyRocketChat("Failed to tag ${imageName} for deployment to ${tag}.")
+        throw(err)
+      }
+
+      notifyRocketChat("Successfully tagged ${imageName} for deployment to ${tag}.")
     }
   }
 }
