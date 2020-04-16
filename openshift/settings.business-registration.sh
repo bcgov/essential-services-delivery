467ac279bbb64251534079b231e43d187939b3e2
Wade Barnes <wade.barnes@shaw.ca>
2020-04-16 07:43:56 -0700

    Switch issuer profiles to use "include"
    - Add a description to all profiles.
    
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

--- openshift/settings.business-registration.sh
+++ openshift/settings.business-registration.sh
@@ -1,2 +1,3 @@
+# Description: Essential Services Registry application profile
 export SKIP_PIPELINE_PROCESSING=1
 export include_templates="esr-web-base-build esr-web-build esr-web-deploy"
--- openshift/settings.esr1.sh
+++ openshift/settings.esr1.sh
@@ -1,2 +1,3 @@
+# Description: Credential issuer profile
 export SKIP_PIPELINE_PROCESSING=1
-export ignore_templates="visual-verifier-build visual-verifier-deploy esr-web-base-build esr-web-build esr-web-deploy"
+export include_templates="agent-build agent-deploy api-build api-deploy db-build db-deploy issuer-web-base-image-build issuer-web-build issuer-web-deploy wallet-build wallet-deploy"
