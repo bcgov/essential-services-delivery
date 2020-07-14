ef96098c121472f9732a4a01e3972961ed2df16c
Emiliano Suñé <emiliano.sune@gmail.com>
2020-07-14 16:44:22 -0700

    Update openvp issuer form
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- openshift/templates/issuer-web/config/openvp/custom-scripts.js
+++ openshift/templates/issuer-web/config/openvp/custom-scripts.js
@@ -13,29 +13,7 @@ function setCurrentISODate(params) {
   survey.setValue(dateField, date.toISOString());
 }
 
-function setPHN(params) {
-  if (params.length < 1) {
-    throw new Error("setExpiryDate is missing one or more required parameters");
-  }
-  var phnField = params[0];
-  var survey = this.survey;
-
-  // Credit to: https://www.w3resource.com/javascript-exercises/javascript-math-exercise-23.php
-  var create_UUID = function () {
-    var dt = new Date().getTime();
-    var uuid = "xxxx xxx xxx".replace(
-      /[xy]/g,
-      function (c) {
-        var r = Math.floor(Math.random() * Math.floor(9))
-        return r;
-      }
-    );
-    return uuid;
-  };
-  survey.setValue(phnField, create_UUID());
-}
-
 /* An array containing custom functions that will be automatically registered with
- * SurveyJS so that they can be used in triggers.
- */
-surveyFunctions = [setCurrentISODate, setPHN];
+* SurveyJS so that they can be used in triggers.
+*/
+surveyFunctions = [setCurrentISODate];
