3e1683051e72115efa2271d8d6c66fe6fc9ddcc1
wadeking98 <wkingnumber2@gmail.com>
2020-12-22 15:19:36 -0800

    deployment config working
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

--- src/bc/baseConfig.groovy
+++ src/bc/baseConfig.groovy
@@ -9,7 +9,7 @@ class baseConfig {
   public static final String[] DEPLOYMENT_ENVIRONMENT_TAGS = ['dev', 'test', 'prod']
 
   // The name of the project namespace(s).
-  public static final String  NAME_SPACE = 'bztwou'
+  public static final String  NAME_SPACE = '4a9599'
 
   // Instance Suffix
   public static final String  SUFFIX = ''
