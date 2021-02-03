c644643773c3a1fbbc9de10e1ba3d4dab170fb59
wadeking98 <wkingnumber2@gmail.com>
2021-02-03 14:43:52 -0800

    changes requested to the PR
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

--- openshift/settings.bcbank.sh
+++ openshift/settings.bcbank.sh
@@ -2,12 +2,3 @@
 export PROJECT_NAMESPACE="a99fd4"
 export SKIP_PIPELINE_PROCESSING=1
 export include_templates="visual-verifier-build visual-verifier-deploy"
-
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# Override the DEV, TEST, PROD deployment environment
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# export TOOLS="devex-von-image-tools"
-# export DEPLOYMENT_ENV_NAME="tools"
-# export DEV="tools"
-# export TEST="tools"
-# export PROD="tools"
--- openshift/settings.bcreg.sh
+++ openshift/settings.bcreg.sh
@@ -2,12 +2,3 @@
 export PROJECT_NAMESPACE="a99fd4"
 export SKIP_PIPELINE_PROCESSING=1
 export include_templates="agent-build agent-deploy api-build api-deploy db-build db-deploy issuer-web-base-image-build issuer-web-build issuer-web-deploy"
-
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# Override the DEV, TEST, PROD deployment environment
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# export TOOLS="devex-von-image-tools"
-# export DEPLOYMENT_ENV_NAME="tools"
-# export DEV="tools"
-# export TEST="tools"
-# export PROD="tools"
