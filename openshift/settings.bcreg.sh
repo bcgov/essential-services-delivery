592c0303082ae7c035b7d355ad7adc2c94ac4602
wadeking98 <wkingnumber2@gmail.com>
2021-02-02 16:44:16 -0800

    multi agent wallet pod up and running
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

--- openshift/settings.bcreg.sh
+++ openshift/settings.bcreg.sh
@@ -1,7 +1,7 @@
 # Description: BC Registries credential issuer main profile
 export PROJECT_NAMESPACE="a99fd4"
 export SKIP_PIPELINE_PROCESSING=1
-export include_templates="agent-build agent-deploy api-build api-deploy db-build db-deploy issuer-web-base-image-build issuer-web-build issuer-web-deploy wallet-build wallet-deploy"
+export include_templates="agent-build agent-deploy api-build api-deploy db-build db-deploy issuer-web-base-image-build issuer-web-build issuer-web-deploy"
 
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 # Override the DEV, TEST, PROD deployment environment
