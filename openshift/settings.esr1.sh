0e4366b447773f3d1b0612f90f3796b77c172865
wadeking98 <wkingnumber2@gmail.com>
2021-02-08 18:54:02 -0800

    joined the database into 1 pvc to save resources
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

--- openshift/settings.esr1.sh
+++ openshift/settings.esr1.sh
@@ -1,3 +1,3 @@
 # Description: Credential issuer profile
 export SKIP_PIPELINE_PROCESSING=1
-export include_templates="agent-build agent-deploy api-build api-deploy db-build db-deploy issuer-web-base-image-build issuer-web-build issuer-web-deploy"
+export include_templates="agent-build agent-deploy api-build api-deploy issuer-web-base-image-build issuer-web-build issuer-web-deploy"
