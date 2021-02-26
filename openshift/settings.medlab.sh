935ff346a3a56521c1879ff503d4c2297f9cedb0
Emiliano Suñé <emiliano.sune@gmail.com>
2021-02-26 13:52:43 -0800

    Reorganized profiles
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- openshift/settings.medlab.sh
+++ openshift/settings.medlab.sh
@@ -1,3 +1,3 @@
 # Description: Credential issuer profile
 export SKIP_PIPELINE_PROCESSING=1
-export include_templates="agent-build agent-deploy api-build api-deploy issuer-web-base-image-build issuer-web-build issuer-web-deploy"
+export include_templates="agent-deploy api-deploy issuer-web-deploy"
--- openshift/settings.safe-entry.sh
+++ openshift/settings.safe-entry.sh
@@ -1,3 +1,3 @@
 # Description: Visual verifier profile
 export SKIP_PIPELINE_PROCESSING=1
-export include_templates="visual-verifier-build visual-verifier-deploy"
+export include_templates="visual-verifier-deploy"
