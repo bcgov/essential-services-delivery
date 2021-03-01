85de55a4f3939b75281202e66555849372a1ace8
Emiliano Suñé <emiliano.sune@gmail.com>
2021-03-01 11:04:42 -0800

    Tweak profile organization
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -1,4 +1,4 @@
 export PROJECT_NAMESPACE="a99fd4"
 export GIT_URI="https://github.com/bcgov/essential-services-delivery.git"
 export GIT_REF="master"
-export include_templates="agent-build api-build issuer-web-base-image-build issuer-web-build visual-verifier-build"
+export include_templates="agent-build api-build db-build db-deploy esr-web-base-build esr-web-build issuer-web-base-image-build issuer-web-build safe-entry-base-build safe-entry-build visual-verifier-build wallet-build wallet-deploy"
