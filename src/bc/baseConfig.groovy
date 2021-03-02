83b4a8f0975af9c216520683a9a2c57cd3d85211
Emiliano Suñé <emiliano.sune@gmail.com>
2021-03-02 10:36:00 -0800

    Fix deployment pipeline
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- src/bc/baseConfig.groovy
+++ src/bc/baseConfig.groovy
@@ -18,5 +18,5 @@ class baseConfig {
   public static final String[] APPS = ['issuer-kit-db', 'issuer-kit-wallet', 'visual-verifier', 'issuer-kit-web', 'issuer-kit-agent', 'issuer-kit-api']
 
   // Apps - Listed in the order they should be tagged for deployment
-  public static final String[] DEPLOY_APPS = ['issuer-kit-db', 'issuer-kit-wallet', 'esr-web', 'safe-entry', 'visual-verifier', 'issuer-kit-web', 'agent', 'issuer-kit-api']
+  public static final String[] DEPLOY_APPS = ['issuer-kit-db', 'issuer-kit-wallet', 'esr-web', 'safe-entry', 'visual-verifier', 'issuer-kit-web', 'issuer-kit-agent', 'issuer-kit-api']
 }
