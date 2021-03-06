3444190d65d56046490441b369a1220cf1f54211
Emiliano Suñé <emiliano.sune@gmail.com>
2021-03-05 16:48:04 -0800

    Update pipelines with new component names
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- jenkins/agent/config.groovy
+++ jenkins/agent/config.groovy
@@ -2,7 +2,7 @@
 // Import this file into the pipeline using 'load'.
 class config extends bc.baseConfig {
   // Build configuration
-  public static final String  APP_NAME = "agent"
+  public static final String  APP_NAME = "issuer-kit-agent"
   public static final String[] BUILDS = ["${this.APP_NAME}"]
 }
 
